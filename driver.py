7d9b6b908b802d4822203228c1794da123bcd3a4
Adam L. More <AdamLMore@users.noreply.github.com>
2016-03-24 02:40:34 -0700

    Restructured code, made sitesVisited tracking sets global, write sites visited to file in UTF-8 format

--- driver.py
+++ driver.py
@@ -1,44 +1,54 @@
 # Provides main driver code for the zite crawler.
 
-
 import krawler
 import sys
+import thread
+import threading
 from Queue import Queue
 from sets import Set
 from selenium import webdriver
 
 
+MAX_PATHS = 50 #Max new paths to branch to
+ZITE_PATH_FILE = "Paths.dat"
+ZITE_ALL_PATHS_FILE = "AllPaths.dat"
+MAX_THREADS = 10
+urlsVisited = Set()
+pathsVisited = Set()
 
-def crawl(path, pathsVisited):
+def crawl(path, pathsVisited, ziteAllPathsFile, zitePathFile):
     print krawler.BASE_URL + path
-    urlsVisited = Set()
     urlsToVisit = Queue()
     urlsToVisit.put(path)
     while not urlsToVisit.empty():
         url = urlsToVisit.get()
         if url not in urlsVisited:
             urlsVisited.add(url)
-            print "Visiting: " + url
-            #url = krawler.resolveURL(url)
+            print "URLs Visited: " + str(len(urlsVisited))
+            #print "Visiting: " + url
             paths = krawler.getPathsStruct(url)
             for link in paths.OtherPaths:
-                print "--" + link
+                #print "--" + link
                 p = path+"/"+link
                 if p not in urlsVisited:
+                    ziteAllPathsFile.write(p.decode('UTF-8') + u"\n")
                     urlsToVisit.put(p)
-                else:
-                    print "---- already visited"
+                #else:
+                    #print "---- already visited"
 
-            if len(pathsVisited) < 25:
-                for link in paths.SlashPaths:
-                    if link not in pathsVisited:
-                        if len(pathsVisited) < 25:
-                            pathsVisited.add(link)
-                            uToAdd = crawl(link, pathsVisited)
-                            for u in uToAdd:
-                                urlsVisited.add(u)
-            else:
-                print "Max Paths Reached! " + str(len(pathsVisited))
+        if len(pathsVisited) < MAX_PATHS:
+            if len(paths.SlashPaths) > 0:
+                print "SlashPaths found: " + str(len(paths.SlashPaths))
+            for link in paths.SlashPaths:
+                if link not in pathsVisited:
+                    print " > " + link
+                    print "Paths Visited: " + str(len(pathsVisited)) + "/" + str(MAX_PATHS)
+                    if len(pathsVisited) < MAX_PATHS:
+                        zitePathFile.write(link.decode('UTF-8')+u"\n")
+                        pathsVisited.add(link)
+                        crawl(link, pathsVisited, ziteAllPathsFile, ziteAllPathsFile)
+        else:
+            print "Max Paths Reached! " + str(len(pathsVisited))
     return urlsVisited
 
 if __name__ == "__main__":
@@ -46,8 +56,9 @@ if __name__ == "__main__":
         path = sys.argv[1] 
         pathsVisited = Set()
         pathsVisited.add(path)
-
-        crawledLinks = crawl(path, pathsVisited)
+        zitePathFile = open(ZITE_PATH_FILE, 'a')
+        ziteAllPathsFile = open(ZITE_ALL_PATHS_FILE, 'a')
+        crawledLinks = crawl(path, pathsVisited, ziteAllPathsFile, zitePathFile)
 
         for link in crawledLinks:
             print " * " + link
