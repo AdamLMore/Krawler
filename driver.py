e831cb107d2f28d1542c0bec2a3aa6783fd5d445
Adam L. More <AdamLMore@users.noreply.github.com>
2016-03-24 17:39:38 -0700

    Removed forward slashes from end of paths to prevent double-forward-slashes from appearing in URL formation

--- driver.py
+++ driver.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 
 
 MAX_PATHS = 50 #Max new paths to branch to
-MAX_URLS  = 350
+MAX_URLS  = 350#Max URLs to visit
 ZITE_PATH_FILE = "Paths.dat"
 ZITE_ALL_PATHS_FILE = "AllPaths.dat"
 MAX_THREADS = 10
@@ -24,15 +24,22 @@ def crawl(path, pathsVisited, ziteAllPathsFile, zitePathFile):
     urlsToVisit.put(path)
     while not urlsToVisit.empty() and len(urlsVisited) < MAX_URLS:
         url = urlsToVisit.get()
+
+        if url.endswith("/"):
+            url = url[:-1]
+
         if url not in urlsVisited:
             urlsVisited.add(url)
             ziteAllPathsFile.write(url.encode('UTF-8') + u"\n")
             print "URLs Visited: " + str(len(urlsVisited))
             #print "Visiting: " + url
             paths = krawler.getPathsStruct(url)
+            print str(len(paths.HTTPPaths)) + " " + str(len(paths.OtherPaths)) + " " + str(len(paths.SlashPaths))
             for link in paths.OtherPaths:
                 #print "--" + link
                 p = path+"/"+link
+                if p.endswith("/"):
+                    p = p[:-1]
                 if p not in urlsInQueue:
                     urlsInQueue.add(p)
                     urlsToVisit.put(p)
@@ -44,6 +51,8 @@ def crawl(path, pathsVisited, ziteAllPathsFile, zitePathFile):
                 if len(paths.SlashPaths) > 0:
                     print "SlashPaths found: " + str(len(paths.SlashPaths))
                 for link in paths.SlashPaths:
+                    if link.endswith("/"):
+                        link = link[:-1]
                     if link not in pathsVisited:
                         print " > " + link
                         print "Paths Visited: " + str(len(pathsVisited)) + "/" + str(MAX_PATHS)
